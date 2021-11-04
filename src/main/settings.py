43b89f96ba08d48a8b49ed3746618e3f1b64c0dc
Niels Lensink <nielslensink@gmail.com>
2021-11-04 14:49:51 +0100

    Collectstatic fixed

--- src/main/settings.py
+++ src/main/settings.py
@@ -184,9 +184,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_URL = os.path.join(BASE_URL, 'static/')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    'templates/static',
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
