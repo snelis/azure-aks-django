8ea10653219b74f2257dfdf6cf0616036e37c1b5
Niels Lensink <nielslensink@gmail.com>
2021-11-10 17:18:14 +0100

    Lintfix

--- src/api/info/views.py
+++ src/api/info/views.py
@@ -7,6 +7,7 @@ logger = logging.getLogger(__name__)
 
 from time import sleep
 
+
 class InfoViewSet(viewsets.ViewSet):
     def list(self, request, *args, **kwargs):
         data = dict(hello='world')
